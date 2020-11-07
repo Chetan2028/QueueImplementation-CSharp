e9a86eba5612cabba7027d6627fab61e2fed2b32
Chetan2028 <bmchetan2028@gmail.com>
2020-11-07 07:20:42 +0530

    Add project files.

---
+++ QueueImplementation/LinkedListQueue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueueImplementation
+{
+    class LinkedListQueue
+    {
+        Node head = null;
+
+        public void Enqueue()
+        {
+
+        }
+    }
+}
---
+++ QueueImplementation/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueueImplementation
+{
+    public class Node
+    {
+        int data;
+        Node next;
+
+        public Node(int data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
+}
